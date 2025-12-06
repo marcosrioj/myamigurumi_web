686b087b9bfa2ffad8d4c4f4f75a7899baaa06da
Marcos Lima <marcosrioj@gmail.com>
2025-12-06 15:30:58 -0800

    eslint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,23 @@
-import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
-  js.configs.recommended,
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/bin/**', '**/obj/**']
+  },
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
-    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/bin/**', '**/obj/**'],
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: { jsx: true }
-      }
+      parserOptions: { ecmaFeatures: { jsx: true } }
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
-      react,
-      'react-hooks': reactHooks
+      '@typescript-eslint': tsPlugin
     },
     rules: {
-      ...tsPlugin.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      'react/prop-types': 'off'
-    },
-    settings: {
-      react: { version: 'detect' }
+      ...tsPlugin.configs.recommended.rules
     }
   }
 ];
